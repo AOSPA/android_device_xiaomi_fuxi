d9dc2ff052538bae01703b4870a39c1d007d4b88
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-11-07 09:05:59 +0100

    fuxi: Add non common blob fixups
    Change-Id: I5efc75531e9bd093f82020d88c57190efec983e7

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,28 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        odm/etc/camera/enhance_motiontuning.xml |odm/etc/camera/night_motiontuning.xml | odm/etc/camera/motiontuning.xml)
+            [ "$2" = "" ] && return 0
+            sed -i 's/<?xml=/<?xml /g' "${2}"
+            ;;
+        odm/lib64/libailab_rawhdr.so | odm/lib64/libxmi_high_dynamic_range_cdsp.so)
+            [ "$2" = "" ] && return 0
+            "${ANDROID_ROOT}"/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip --strip-debug "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
