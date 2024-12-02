cea99c8d0df1af7447c74aec5a40a4d84aff443e
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-12-02 15:01:59 +0100

    fuxi: Fixup "Rework device kernel modules handling"
    Change-Id: I57cbcfb924011c7494793983b670fc2a5276611f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,9 +14,10 @@ TARGET_SCREEN_DENSITY := 420
 
 # Kernel Modules
 device_second_stage_modules := \
-    fts_touch_spi.ko \
-    xiaomi_touch.ko
+    xiaomi_touch.ko \
+    fts_touch_spi.ko
 
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_PREBUILT_DIR)/vendor_dlkm/, $(device_second_stage_modules))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
 
