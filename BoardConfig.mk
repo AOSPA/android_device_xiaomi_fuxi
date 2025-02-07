06877b495aa936ccbcb43f9324197070e1ffe077
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-07 23:42:02 +0100

    fuxi: Update to OS2.0.2.0.VMCMIXM
    Change-Id: I1c183ae0c7a08b97f283b9a0d54d8f4199fbaff6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,10 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# HIDL
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest_fuxi.xml
+
 # OTA
 TARGET_OTA_ASSERT_DEVICE := fuxi
 
