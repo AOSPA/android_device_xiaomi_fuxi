b925505d9ff14f50eddb2799a7d481a8a633edc4
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-04-23 15:27:41 +0200

    fuxi: Add support for ESIM (eUICC)
    Change-Id: I3484dafe338fd3448815657447e5901534eb60c4

--- device.mk
+++ device.mk
@@ -24,3 +24,7 @@ PRODUCT_PACKAGES += \
     FuxiSystemUI \
     FuxiWifiRes \
     FuxiWifiResMainline \
+
+# Telephony
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
